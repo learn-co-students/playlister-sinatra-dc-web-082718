20592bc1a492714ab37358afa0737d786051eb85
github username <github email address>
2018-09-20 17:47:30 -0400

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
-require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
-  use Rack::Flash
-  enable :sessions
+
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
